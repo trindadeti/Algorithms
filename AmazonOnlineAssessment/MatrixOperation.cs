d96bb8f62f0fd8319373b8538bc3b90ea0b91a3f
Tiago Trindade <trindadeti@gmail.com>
2019-10-01 00:33:56 -0300

    mine_sweeper Method

---
+++ AmazonOnlineAssessment/MatrixOperation.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace AmazonOnlineAssessment
+{
+    public class MatrixOperation
+    {
+        public int[,] mineSweeper(int[][] bombs, int RowNumbers, int CollumnNumbers)
+        {
+            int[,] fieldMatrix = new int[RowNumbers, CollumnNumbers];
+
+            foreach (int[] bomb in bombs)
+            {
+                int x = bomb[0];
+                int y = bomb[1];
+                fieldMatrix[x, y] = -1;
+
+                for (int i = x - 1; i <= x + 1; i++)
+                {
+                    for (int j = y - 1; j <= y + 1; j++)
+                    {
+                        if (i >= 0 && i < RowNumbers
+                        && j >= 0 && j < CollumnNumbers
+                        && fieldMatrix[i, j] != -1)
+                        {
+                            fieldMatrix[i, j]++;
+                        }
+                    }
+                }
+            }
+            
+            return fieldMatrix;
+        }
+
+    }
+}
---
+++ UnitTestProject/MatrixOperationTest.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using AmazonOnlineAssessment;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTestProject
+{
+    [TestClass]
+    public class MatrixOperationTest
+    {
+
+        [TestMethod]
+        public void validate_mine_sweeper()
+        {
+            //Arrange
+            int[][] bombs = new int[2][];
+            int[] bomb1 = new int[2] { 0, 0 };
+            int[] bomb2 = new int[2] { 0, 1 };
+
+            bombs[0] = bomb1;
+            bombs[1] = bomb2;
+
+            //Act
+            MatrixOperation matrixOperation = new MatrixOperation();
+            int[,] result = matrixOperation.mineSweeper(bombs, 3, 4);
+
+            //Assert
+            Assert.AreEqual(result[0, 0], -1);
+            Assert.AreEqual(result[0, 1], -1);
+            Assert.AreEqual(result[0, 2], 1);
+            Assert.AreEqual(result[0, 3], 0);
+            Assert.AreEqual(result[1, 0], 2);
+            Assert.AreEqual(result[1, 1], 2);
+            Assert.AreEqual(result[1, 2], 1);
+            Assert.AreEqual(result[1, 3], 0);
+            Assert.AreEqual(result[2, 0], 0);
+            Assert.AreEqual(result[2, 1], 0);
+            Assert.AreEqual(result[2, 2], 0);
+            Assert.AreEqual(result[2, 3], 0);
+            
+        }
+
+    }
+}
